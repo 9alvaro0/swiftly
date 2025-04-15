972d07cfdc7372048e69d52596f2bd80902fc6e8
Alvaro Guerra <9alvaro0@gmail.com>
2025-04-15 18:14:06 +0200

    Adds Vercel Speed Insights for performance monitoring
    Implements Vercel Speed Insights to gain better insights
    into the application's performance and identify areas for
    improvement.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,22 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.css";
-import MainLayout from '@/components/layout/MainLayout';
-const inter = Inter({ subsets: ['latin'] });
+import MainLayout from "@/components/layout/MainLayout";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Swiftly - Tutoriales de Swift y SwiftUI',
-  description: 'Blog, guías y tutoriales para aprender Swift y SwiftUI de manera efectiva',
+    title: "Swiftly - Tutoriales de Swift y SwiftUI",
+    description: "Blog, guías y tutoriales para aprender Swift y SwiftUI de manera efectiva",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="es">
-      <body className={inter.className}>
-        <MainLayout>{children}</MainLayout>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="es">
+            <body className={inter.className}>
+                <MainLayout>{children}</MainLayout>
+                <SpeedInsights />
+            </body>
+        </html>
+    );
 }
