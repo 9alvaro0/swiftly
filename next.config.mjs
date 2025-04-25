c76e5d1cf4ac08ff6f9ddb0ad932b69eeaa7af4a
Alvaro Guerra <102147050+9alvaro0@users.noreply.github.com>
2025-04-25 12:57:02 +0200

    chore: update next.config.mjs with enhanced configuration

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['placehold.co'],
+  },
+};
+
+export default nextConfig;
